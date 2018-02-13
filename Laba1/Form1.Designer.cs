15f86bc9df9890872c004742641132cada853aa9
михаил юрков <mishayur1997@mail.ru>
2018-02-14 00:50:17 +0500

    Добавил весь проект

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataSet1 = new System.Data.DataSet();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nUD_pol = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nUD_long = new System.Windows.Forms.NumericUpDown();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.nUD_hight = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cb_rast = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.l_name = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.nUD_her = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_pol)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_long)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_hight)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_her)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nUD_pol);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(180, 64);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sepal length";
+            // 
+            // nUD_pol
+            // 
+            this.nUD_pol.Location = new System.Drawing.Point(20, 23);
+            this.nUD_pol.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nUD_pol.Name = "nUD_pol";
+            this.nUD_pol.Size = new System.Drawing.Size(133, 22);
+            this.nUD_pol.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nUD_long);
+            this.groupBox2.Location = new System.Drawing.Point(16, 158);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(180, 64);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Petal length";
+            // 
+            // nUD_long
+            // 
+            this.nUD_long.Location = new System.Drawing.Point(20, 23);
+            this.nUD_long.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nUD_long.Name = "nUD_long";
+            this.nUD_long.Size = new System.Drawing.Size(133, 22);
+            this.nUD_long.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.nUD_hight);
+            this.groupBox3.Location = new System.Drawing.Point(16, 86);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(180, 64);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sepal width";
+            // 
+            // nUD_hight
+            // 
+            this.nUD_hight.Location = new System.Drawing.Point(20, 23);
+            this.nUD_hight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nUD_hight.Name = "nUD_hight";
+            this.nUD_hight.Size = new System.Drawing.Size(133, 22);
+            this.nUD_hight.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 106);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Распознать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cb_rast
+            // 
+            this.cb_rast.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_rast.FormattingEnabled = true;
+            this.cb_rast.Items.AddRange(new object[] {
+            "Евклидово",
+            "По Хеммингу",
+            "Латинских кварталов"});
+            this.cb_rast.Location = new System.Drawing.Point(23, 23);
+            this.cb_rast.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cb_rast.Name = "cb_rast";
+            this.cb_rast.Size = new System.Drawing.Size(132, 24);
+            this.cb_rast.TabIndex = 4;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cb_rast);
+            this.groupBox4.Location = new System.Drawing.Point(233, 15);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Size = new System.Drawing.Size(180, 64);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Количество полосок";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.l_name);
+            this.groupBox5.Location = new System.Drawing.Point(233, 158);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox5.Size = new System.Drawing.Size(180, 64);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Ваш образец это";
+            // 
+            // l_name
+            // 
+            this.l_name.AutoSize = true;
+            this.l_name.Location = new System.Drawing.Point(35, 26);
+            this.l_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.l_name.Name = "l_name";
+            this.l_name.Size = new System.Drawing.Size(0, 17);
+            this.l_name.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.nUD_her);
+            this.groupBox6.Location = new System.Drawing.Point(16, 230);
+            this.groupBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Size = new System.Drawing.Size(180, 64);
+            this.groupBox6.TabIndex = 6;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Petal width";
+            // 
+            // nUD_her
+            // 
+            this.nUD_her.Location = new System.Drawing.Point(20, 23);
+            this.nUD_her.Margin = new System.Windows.Forms.Padding(4);
+            this.nUD_her.Name = "nUD_her";
+            this.nUD_her.Size = new System.Drawing.Size(133, 22);
+            this.nUD_her.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 347);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Распознавание животных";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_pol)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_long)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_hight)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nUD_her)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nUD_pol;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown nUD_long;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown nUD_hight;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cb_rast;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label l_name;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.NumericUpDown nUD_her;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        DataSet Animals;
+        double[,] tvoia_mamka =
+       {{5.1, 3.5, 1.4, 0.2},
+        {4.9, 3.0, 1.4, 0.2},
+        {4.7, 3.2, 1.3, 0.2},
+        {4.6, 3.1, 1.5, 0.2},
+        {5.0, 3.6, 1.4, 0.2},
+        {5.4, 3.9, 1.7, 0.4},
+        {4.6, 3.4, 1.4, 0.3},
+        {5.0, 3.4, 1.5, 0.2},
+        {4.4, 2.9, 1.4, 0.2},
+        {4.9, 3.1, 1.5, 0.1},
+        {5.4, 3.7, 1.5, 0.2},
+        {4.8, 3.4, 1.6, 0.2},
+        {4.8, 3.0, 1.4, 0.1},
+        {4.3, 3.0, 1.1, 0.1},
+        {5.8, 4.0, 1.2, 0.2},
+        {5.7, 4.4, 1.5, 0.4},
+        {5.4, 3.9, 1.3, 0.4},
+        {5.1, 3.5, 1.4, 0.3},
+        {5.7, 3.8, 1.7, 0.3},
+        {5.1, 3.8, 1.5, 0.3},
+        {5.4, 3.4, 1.7, 0.2},
+        {5.1, 3.7, 1.5, 0.4},
+        {4.6, 3.6, 1.0, 0.2},
+        {5.1, 3.3, 1.7, 0.5},
+        {4.8, 3.4, 1.9, 0.2},
+        {5.0, 3.0, 1.6, 0.2},
+        {5.0, 3.4, 1.6, 0.4},
+        {5.2, 3.5, 1.5, 0.2},
+        {5.2, 3.4, 1.4, 0.2},
+        {4.7, 3.2, 1.6, 0.2},
+        {4.8, 3.1, 1.6, 0.2},
+        {5.4, 3.4, 1.5, 0.4},
+        {5.2, 4.1, 1.5, 0.1},
+        {5.5, 4.2, 1.4, 0.2},
+        {4.9, 3.1, 1.5, 0.1},
+        {5.0, 3.2, 1.2, 0.2},
+        {5.5, 3.5, 1.3, 0.2},
+        {4.9, 3.1, 1.5, 0.1},
+        {4.4, 3.0, 1.3, 0.2},
+        {5.1, 3.4, 1.5, 0.2},
+        {5.0, 3.5, 1.3, 0.3},
+        {4.5, 2.3, 1.3, 0.3},
+        {4.4, 3.2, 1.3, 0.2},
+        {5.0, 3.5, 1.6, 0.6},
+        {5.1, 3.8, 1.9, 0.4},
+        {4.8, 3.0, 1.4, 0.3},
+        {5.1, 3.8, 1.6, 0.2},
+        {4.6, 3.2, 1.4, 0.2},
+        {5.3, 3.7, 1.5, 0.2},
+        {5.0, 3.3, 1.4, 0.2}};
+
+        double[,] tvoia_mamka_takaia =
+            {{7.0, 3.2,    4.7,    1.4},
+        {6.4,   3.2,    4.5,    1.5},
+        {6.9,   3.1,    4.9,    1.5},
+        {5.5,   2.3,    4.0,    1.3},
+        {6.5,   2.8,    4.6,    1.5},
+        {5.7,   2.8,    4.5,    1.3},
+        {6.3,   3.3,    4.7,    1.6},
+        {4.9,   2.4,    3.3,    1.0},
+        {6.6,   2.9,    4.6,    1.3},
+        {5.2,   2.7,    3.9,    1.4},
+        {5.0,   2.0,    3.5,    1.0},
+        {5.9,   3.0,    4.2,    1.5},
+        {6.0,   2.2,    4.0,    1.0},
+        {6.1,   2.9,    4.7,    1.4},
+        {5.6,   2.9,    3.6,    1.3},
+        {6.7,   3.1,    4.4,    1.4},
+        {5.6,   3.0,    4.5,    1.5},
+        {5.8,   2.7,    4.1,    1.0},
+        {6.2,   2.2,    4.5,    1.5},
+        {5.6,   2.5,    3.9,    1.1},
+        {5.9,   3.2,    4.8,    1.8},
+        {6.1,   2.8,    4.0,    1.3},
+        {6.3,   2.5,    4.9,    1.5},
+        {6.1,   2.8,    4.7,    1.2},
+        {6.4,   2.9,    4.3,    1.3},
+        {6.6,   3.0,    4.4,    1.4},
+        {6.8,   2.8,    4.8,    1.4},
+        {6.7,   3.0,    5.0,    1.7},
+        {6.0,   2.9,    4.5,    1.5},
+        {5.7,   2.6,    3.5,    1.0},
+        {5.5,   2.4,    3.8,    1.1},
+        {5.5,   2.4,    3.7,    1.0},
+        {5.8,   2.7,    3.9,    1.2},
+        {6.0,   2.7,    5.1,    1.6},
+        {5.4,   3.0,    4.5,    1.5},
+        {6.0,   3.4,    4.5,    1.6},
+        {6.7,   3.1,    4.7,    1.5},
+        {6.3,   2.3,    4.4,    1.3},
+        {5.6,   3.0,    4.1,    1.3},
+        {5.5,   2.5,    4.0,    1.3},
+        {5.5,   2.6,    4.4,    1.2},
+        {6.1,   3.0,    4.6,    1.4},
+        {5.8,   2.6,    4.0,    1.2},
+        {5.0,   2.3,    3.3,    1.0},
+        {5.6,   2.7,    4.2,    1.3},
+        {5.7,   3.0,    4.2,    1.2},
+        {5.7,   2.9,    4.2,    1.3},
+        {6.2,   2.9,    4.3,    1.3},
+        {5.1,   2.5,    3.0,    1.1},
+        {5.7,   2.8,    4.1,    1.3}};
+        double[,] tvoia_mamka_takaia_neochen =
+            {
+        {6.3, 3.3, 6.0, 2.5 },
+        {5.8, 2.7, 5.1, 1.9 },
+        {7.1, 3.0, 5.9, 2.1 },
+        {6.3, 2.9, 5.6, 1.8 },
+        {6.5, 3.0, 5.8, 2.2 },
+        {7.6, 3.0, 6.6, 2.1 },
+        {4.9, 2.5, 4.5, 1.7 },
+        {7.3, 2.9, 6.3, 1.8 },
+        {6.7, 2.5, 5.8, 1.8 },
+        {7.2, 3.6, 6.1, 2.5 },
+        {6.5, 3.2, 5.1, 2.0 },
+        {6.4, 2.7, 5.3, 1.9 },
+        {6.8, 3.0, 5.5, 2.1 },
+        {5.7, 2.5, 5.0, 2.0 },
+        {5.8, 2.8, 5.1, 2.4 },
+        {6.4, 3.2, 5.3, 2.3 },
+        {6.5, 3.0, 5.5, 1.8 },
+        {7.7, 3.8, 6.7, 2.2 },
+        {7.7, 2.6, 6.9, 2.3 },
+        {6.0, 2.2, 5.0, 1.5 },
+        {6.9, 3.2, 5.7, 2.3 },
+        {5.6, 2.8, 4.9, 2.0 },
+        {7.7, 2.8, 6.7, 2.0 },
+        {6.3, 2.7, 4.9, 1.8 },
+        {6.7, 3.3, 5.7, 2.1 },
+        {7.2, 3.2, 6.0, 1.8 },
+        {6.2, 2.8, 4.8, 1.8 },
+        {6.1, 3.0, 4.9, 1.8 },
+        {6.4, 2.8, 5.6, 2.1 },
+        {7.2, 3.0, 5.8, 1.6 },
+        {7.4, 2.8, 6.1, 1.9 },
+        {7.9, 3.8, 6.4, 2.0 },
+        {6.4, 2.8, 5.6, 2.2 },
+        {6.3, 2.8, 5.1, 1.5 },
+        {6.1, 2.6, 5.6, 1.4 },
+        {7.7, 3.0, 6.1, 2.3 },
+        {6.3, 3.4, 5.6, 2.4 },
+        {6.4, 3.1, 5.5, 1.8 },
+        {6.0, 3.0, 4.8, 1.8 },
+        {6.9, 3.1, 5.4, 2.1 },
+        {6.7, 3.1, 5.6, 2.4 },
+        {6.9, 3.1, 5.1, 2.3 },
+        {5.8, 2.7, 5.1, 1.9 },
+        {6.8, 3.2, 5.9, 2.3 },
+        {6.7, 3.3, 5.7, 2.5 },
+        {6.7, 3.0, 5.2, 2.3 },
+        {6.3, 2.5, 5.0, 1.9 },
+        {6.5, 3.0, 5.2, 2.0 },
+        {6.2, 3.4, 5.4, 2.3 },
+        {5.9, 3.0, 5.1, 1.8 },
+        };
+        public void add_animal(DataTable table,double a1,double a2, double a3, double a4)
+        {
+            table.Rows.Add(a1, a2, a3, a4);
+        }
+        public double Evkl_dist(DataRow row)
+        {            
+            return Math.Sqrt(Math.Pow((double)row.ItemArray[0] - (double)nUD_pol.Value, 2) + Math.Pow((double)row.ItemArray[1] - (double)nUD_hight.Value, 2) + Math.Pow((double)row.ItemArray[2] - (double)nUD_long.Value, 2) + Math.Pow((double)row.ItemArray[3] - (double)nUD_her.Value, 2));
+        }
+        public double Hem_dist(DataRow row)
+        {
+            return Math.Abs((double)row.ItemArray[0] - (double)nUD_pol.Value) + Math.Abs((double)row.ItemArray[1] - (double)nUD_hight.Value) + Math.Abs((double)row.ItemArray[2] - (double)nUD_long.Value) + Math.Abs((double)row.ItemArray[3] - (double)nUD_her.Value);
+        }
+        public double City_kv(DataRow row)
+        {
+            double max = Math.Abs((double)row.ItemArray[0] - (double)nUD_pol.Value);
+            if (max < Math.Abs((double)row.ItemArray[1] - (double)nUD_hight.Value)) max = Math.Abs((double)row.ItemArray[1] - (double)nUD_hight.Value);
+            if (max < Math.Abs((double)row.ItemArray[2] - (double)nUD_long.Value)) max = Math.Abs((double)row.ItemArray[2] - (double)nUD_long.Value);
+            if (max < Math.Abs((double)row.ItemArray[3] - (double)nUD_pol.Value)) max = Math.Abs((double)row.ItemArray[3] - (double)nUD_her.Value);
+            return  max;
+        }
+        public void Choise()
+        {
+            double min_dist = Double.MaxValue, dist = 0;
+            string Name = "";
+            foreach(DataTable tabl in Animals.Tables)
+            {
+                foreach(DataRow row in tabl.Rows)
+                {
+                    if (cb_rast.SelectedIndex == 0)
+                    {
+                        dist = Evkl_dist(row);
+                    }
+                    else
+                        if (cb_rast.SelectedIndex == 1)
+                    {
+                        dist = Hem_dist(row);
+                    }
+                    else
+                        dist = City_kv(row);
+                    if (dist < min_dist)
+                    {
+                        min_dist = dist;
+                        Name = tabl.TableName;
+                    }
+                   
+                }
+            }
+            l_name.Text = Name;
+        }
+        //table.Rows.Add(nUD_pol.Value, nUD_hight.Value, nUD_long.Value);
+        public Form1()
+        {
+            InitializeComponent();
+            cb_rast.SelectedIndex = 0;
+            Animals = new DataSet("Animals");
+
+            DataTable t_Bee = Animals.Tables.Add("Iris-setosa");
+            DataColumn col_bee =
+                t_Bee.Columns.Add("sepal length", typeof(double));
+            t_Bee.Columns.Add("sepal width", typeof(double));
+            t_Bee.Columns.Add("petal length", typeof(double));
+            t_Bee.Columns.Add("petal width", typeof(double));
+            for (int i = 0; i < tvoia_mamka.Length / 4; i++)
+                add_animal(t_Bee, tvoia_mamka[i, 0], tvoia_mamka[i, 1], tvoia_mamka[i, 2], tvoia_mamka[i, 3]);
+
+            DataTable t_zebra = Animals.Tables.Add("Iris-versicolor");
+            DataColumn col_zebra =
+                t_zebra.Columns.Add("sepal length", typeof(double));
+            t_zebra.Columns.Add("sepal width", typeof(double));
+            t_zebra.Columns.Add("petal length", typeof(double));
+            t_zebra.Columns.Add("petal width", typeof(double));
+            for (int i = 0; i < tvoia_mamka_takaia.Length / 4; i++)
+                add_animal(t_zebra, tvoia_mamka_takaia[i, 0], tvoia_mamka_takaia[i, 1], tvoia_mamka_takaia[i, 2], tvoia_mamka_takaia[i, 3]);
+
+            DataTable t_tiger = Animals.Tables.Add("Iris-virginica");
+            DataColumn col_tiger =
+                t_tiger.Columns.Add("sepal length", typeof(double));
+            t_tiger.Columns.Add("sepal width", typeof(double));
+            t_tiger.Columns.Add("petal length", typeof(double));
+            t_tiger.Columns.Add("petal width", typeof(double));
+            for (int i = 0; i < tvoia_mamka_takaia_neochen.Length / 4; i++)
+                add_animal(t_tiger, tvoia_mamka_takaia_neochen[i, 0], tvoia_mamka_takaia_neochen[i, 1], tvoia_mamka_takaia_neochen[i, 2], tvoia_mamka_takaia_neochen[i, 3]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Choise();
+        }
+    }
+}
